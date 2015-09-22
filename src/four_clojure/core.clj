afa551998a52b06e74ac7769defa161cb8daecc1
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-09-22 06:47:51 -0400

    Solve Problem #17

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -98,3 +98,10 @@
   (= ((fn greetify [name] (str "Hello, " name "!")) "Dave") "Hello, Dave!")
   (= ((fn greetify [name] (str "Hello, " name "!")) "Jenn") "Hello, Jenn!")
   (= ((fn greetify [name] (str "Hello, " name "!")) "Rhea") "Hello, Rhea!"))
+
+(defn problem-seventeen
+  "The map function takes two arguments: a function (f) and a sequence (s).
+  Map returns a new sequence consisting of the result of applying f to each item of s.
+  Do not confuse the map function with the map data structure."
+  []
+  (= '(6 7 8) (map #(+ % 5) '(1 2 3))))
