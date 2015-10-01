b3a9ff72aa6dad1bd656e38a9bb7ce832824fd9f
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-09-30 22:15:35 -0400

    Solve Problem #23

--- src/four_clojure/easy.clj
+++ src/four_clojure/easy.clj
@@ -34,3 +34,11 @@
     (= (my-cnt [[1 2] [3 4] [5 6]]) 3)
     (= (my-cnt '(13)) 1)
     (= (my-cnt '(:a :b :c)) 3)))
+
+(defn problem-twenty-three
+  "Write a function which reverses a sequence."
+  []
+  (let [my-rev (fn [coll] (reduce conj () (seq coll)))]
+    (= (my-rev [1 2 3 4 5]) [5 4 3 2 1])
+    (= (my-rev (sorted-set 5 7 2 7)) '(7 5 2))
+    (= (my-rev [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]])))
