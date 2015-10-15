d01da8eb9a250687e12d728c5a02387d836dd630
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-10-15 19:49:32 -0400

    Solve Problem #36

--- src/four_clojure/elementary.clj
+++ src/four_clojure/elementary.clj
@@ -118,3 +118,10 @@
   (= 7 (let [x 5] (+ 2 x)))
   (= 7 (let [x 3, y 10] (- y x)))
   (= 7 (let [x 21] (let [y 3] (/ x y)))))
+
+(defn problem-thirty-six
+  "Can you bind x, y, and z so that these are all true?"
+  []
+  (= 10 (let [x 7 y 3 z 1] (+ x y)))
+  (= 4  (let [x 7 y 3 z 1] (+ y z)))
+  (= 1  (let [x 7 y 3 z 1] z)))
