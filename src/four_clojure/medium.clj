2ae035e235d6d7bb33bb1b61acb04a3cb6e54cf6
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-10-23 07:32:27 -0400

    Solve Problem #44

--- src/four_clojure/medium.clj
+++ src/four_clojure/medium.clj
@@ -7,3 +7,16 @@
     (= (my-rev-interleave [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
     (= (my-rev-interleave (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
     (= (my-rev-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))))
+
+(defn problem-forty-four
+  "Write a function which can rotate a sequence in either direction."
+  []
+  (let [my-rotate (fn [n coll]
+                    (->>
+                      (split-at (mod n (count coll)) coll)
+                      (apply #(concat %2 %1))))]
+    (= (my-rotate 2 [1 2 3 4 5]) '(3 4 5 1 2))
+    (= (my-rotate -2 [1 2 3 4 5]) '(4 5 1 2 3))
+    (= (my-rotate 6 [1 2 3 4 5]) '(2 3 4 5 1))
+    (= (my-rotate 1 '(:a :b :c)) '(:b :c :a))
+    (= (my-rotate -4 '(:a :b :c)) '(:c :a :b))))
