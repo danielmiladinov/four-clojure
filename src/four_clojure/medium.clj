b09f93e781d281d1195c281bee4e5bee69bdabf8
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-10-25 18:23:30 -0400

    Solve Problem #46

--- src/four_clojure/medium.clj
+++ src/four_clojure/medium.clj
@@ -20,3 +20,14 @@
     (= (my-rotate 6 [1 2 3 4 5]) '(2 3 4 5 1))
     (= (my-rotate 1 '(:a :b :c)) '(:b :c :a))
     (= (my-rotate -4 '(:a :b :c)) '(:c :a :b))))
+
+(defn problem-forty-six
+  "Write a higher-order function which flips the order of the arguments of an input function."
+  []
+  (let [flip-args (fn [f]
+                    (fn flipped [& args]
+                      (apply f (reverse args))))]
+    (= 3 ((flip-args nth) 2 [1 2 3 4 5]))
+    (= true ((flip-args >) 7 8))
+    (= 4 ((flip-args quot) 2 8))
+    (= [1 2 3] ((flip-args take) [1 2 3 4 5] 3))))
