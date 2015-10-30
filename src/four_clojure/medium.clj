30ba2d42ea9e8ba2336185ccaf5430b2ce2a50ae
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-10-30 07:54:23 -0400

    Solve Problem #50 with a much shorter function
    My previous solution to #50 basically re-invented the functions vals and
    group-by from first principles. In addition to understanding the
    mechanices of clojure, I need to get better at discovering what
    functions are already available to me.

--- src/four_clojure/medium.clj
+++ src/four_clojure/medium.clj
@@ -37,14 +37,7 @@
    homogeneous sub-sequences. The internal order of each sub-sequence should be maintained, but the sub-sequences
    themselves can be returned in any order (this is why 'set' is used in the test cases)."
   []
-  (let [split-by-type (fn [coll]
-                        (map second
-                          (reduce
-                            (fn [m v]
-                              (update-in
-                                m
-                                [(type v)]
-                                #(conj (or %1 []) v))) {} coll)))]
+  (let [split-by-type (fn [coll] (vals (group-by type coll)))]
     (= (set (split-by-type [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
     (= (set (split-by-type [:a "foo" "bar" :b])) #{[:a :b] ["foo" "bar"]})
     (= (set (split-by-type [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})))
