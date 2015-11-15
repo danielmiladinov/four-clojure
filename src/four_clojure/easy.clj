95e1d56827d2251981e0283cc55db8190e39297e
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-11-14 23:31:17 -0500

    Solve Problem #64

--- src/four_clojure/easy.clj
+++ src/four_clojure/easy.clj
@@ -278,3 +278,14 @@
        {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
     (= (my-group-by count [[1] [1 2] [3] [1 2 3] [2 3]])
        {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})))
+
+(defn problem-sixty-four
+  "Reduce takes a 2 argument function and an optional starting value.
+  It then applies the function to the first 2 items in the sequence (or the starting value
+  and the first element of the sequence). In the next iteration the function will be called
+  on the previous return value and the next item from the sequence, thus reducing the entire collection to one value.
+  Don't worry, it's not as complicated as it sounds."
+  []
+  (= 15 (reduce + [1 2 3 4 5]))
+  (=  0 (reduce + []))
+  (=  6 (reduce + 1 [2 3])))
