7f6bb6986fd22b44791b6446c339e144a3ecf632
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-11-23 18:07:00 -0500

    Solve Problem #73

--- src/four_clojure/hard.clj
+++ src/four_clojure/hard.clj
@@ -25,3 +25,75 @@
     (= (longest-inc-seq [5 6 1 3 2 7]) [5 6])
     (= (longest-inc-seq [2 3 3 4 5]) [3 4 5])
     (= (longest-inc-seq [7 6 5 4]) [])))
+
+(defn problem-seventy-three
+  "A tic-tac-toe board is represented by a two dimensional vector.
+  X is represented by :x, O is represented by :o, and empty is represented by :e.
+  A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal row.
+  Write a function which analyzes a tic-tac-toe board and returns :x if X has won,
+  :o if O has won, and nil if neither player has won."
+  []
+  (let [winner (fn [board]
+                 (let [row   (fn [n b] (b n))
+                       col   (fn [n b] (into [] (map #(% n) b)))
+                       ldiag (fn [b]
+                               (->> (reduce
+                                      (fn [[ld i] r]
+                                        (vector
+                                          (conj ld (r i))
+                                          (inc i)))
+                                      [[] 0] b)
+                                    (first)))
+                       rdiag (fn [b]
+                               (->> (reduce
+                                      (fn [[rd i] r]
+                                        (vector
+                                          (conj rd (r i))
+                                          (dec i)))
+                                      [[] 2] b)
+                                    (first)))
+                       xs    (fn [s] (map #(if (= :x %) 1 0) s))
+                       os    (fn [s] (map #(if (= :o %) 1 0) s))
+                       won   (fn [p]
+                               (->>
+                                 (map
+                                   (fn [f]
+                                     (->>
+                                       (f board)
+                                       (p)
+                                       (apply +)))
+                                   [(partial row 0)
+                                    (partial row 1)
+                                    (partial row 2)
+                                    (partial col 0)
+                                    (partial col 1)
+                                    (partial col 2)
+                                    ldiag
+                                    rdiag])
+                                 (filter #(= 3 %))
+                                 (seq)))]
+                   (cond
+                     (won xs) :x
+                     (won os) :o
+                     :else nil)))]
+    (= nil (winner [[:e :e :e]
+                    [:e :e :e]
+                    [:e :e :e]]))
+    (= :x (winner [[:x :e :o]
+                   [:x :e :e]
+                   [:x :e :o]]))
+    (= :o (winner [[:e :x :e]
+                   [:o :o :o]
+                   [:x :e :x]]))
+    (= nil (winner [[:x :e :o]
+                    [:x :x :e]
+                    [:o :x :o]]))
+    (= :x (winner [[:x :e :e]
+                   [:o :x :e]
+                   [:o :e :x]]))
+    (= :o (winner [[:x :e :o]
+                   [:x :o :e]
+                   [:o :e :x]]))
+    (= nil (winner [[:x :o :x]
+                    [:x :o :x]
+                    [:o :x :o]]))))
