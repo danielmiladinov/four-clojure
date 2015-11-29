f6b41069b5f8d8c58688b1b6b8e5511f78223c92
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-11-29 17:36:59 -0500

    Solve Problem #79

--- src/four_clojure/hard.clj
+++ src/four_clojure/hard.clj
@@ -97,3 +97,31 @@
     (= nil (winner [[:x :o :x]
                     [:x :o :x]
                     [:o :x :o]]))))
+
+(defn problem-seventy-nine
+  "Write a function which calculates the sum of the minimal path through a triangle.
+  The triangle is represented as a collection of vectors.
+  The path should start at the top of the triangle and move to an adjacent number
+  on the next row until the bottom of the triangle is reached."
+  []
+  (let [min-path-sum (fn [triangle]
+                       (->> triangle
+                            (reverse)
+                            (reduce (fn [bot top]
+                                      (->> (partition 2 1 bot)
+                                           (map #(apply min %))
+                                           (map vector top)
+                                           (map #(apply + %))
+                                           (min))))
+                            (first)))]
+    (= 7 (min-path-sum '([1]
+                         [2 4]
+                         [5 1 4]
+                         [2 3 4 5])))
+
+    (= 20 (min-path-sum '([3]
+                          [2 4]
+                          [1 9 3]
+                          [9 9 2 4]
+                          [4 6 6 7 8]
+                          [5 7 3 5 1 4])))))
