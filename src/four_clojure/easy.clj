2dc4c688220c647e55bd66c3e156e84226512216
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-12-07 21:23:51 -0500

    Solve Problem #88

--- src/four_clojure/easy.clj
+++ src/four_clojure/easy.clj
@@ -322,3 +322,16 @@
     (= true (half-true? false true false))
     (= false (half-true? true true true))
     (= true (half-true? true true true false))))
+
+(defn problem-eighty-eight
+  "Write a function which returns the symmetric difference of two sets.
+  The symmetric difference is the set of items belonging to one but not both of the two sets."
+  []
+  (let [symmetric-difference (fn [x y]
+                               (clojure.set/union
+                                 (clojure.set/difference x y)
+                                 (clojure.set/difference y x)))]
+    (= (symmetric-difference #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})
+    (= (symmetric-difference #{:a :b :c} #{}) #{:a :b :c})
+    (= (symmetric-difference #{} #{4 5 6}) #{4 5 6})
+    (= (symmetric-difference #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]})))
