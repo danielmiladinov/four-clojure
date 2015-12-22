be783da878f0fcc00ceaf61e2608d815e02cef40
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-12-22 18:57:40 -0500

    Solve Problem #103

--- src/four_clojure/medium.clj
+++ src/four_clojure/medium.clj
@@ -421,3 +421,29 @@
     (= (into-camel-case "multi-word-key") "multiWordKey")
     (= (into-camel-case "something") "something")
     (= (into-camel-case "leaveMeAlone") "leaveMeAlone")))
+
+(defn problem-one-hundred-three
+  "Given a sequence S consisting of n elements generate all k-combinations of S,
+  i. e. generate all possible sets consisting of k distinct elements taken from S.
+  The number of k-combinations for a sequence is equal to the binomial coefficient."
+  []
+  (let [k-combinations (fn [k S]
+                         (let [power-set (fn [s]
+                                           (reduce (fn [ps e]
+                                                     (->>
+                                                       ps
+                                                       (map #(set (concat #{e} %)))
+                                                       (concat ps)
+                                                       set))
+                                                   #{#{}}
+                                                   s))]
+                           (into #{}
+                                 (filter #(= (count %) k) (power-set S)))))]
+    (= (k-combinations 1 #{4 5 6}) #{#{4} #{5} #{6}})
+    (= (k-combinations 10 #{4 5 6}) #{})
+    (= (k-combinations 2 #{0 1 2}) #{#{0 1} #{0 2} #{1 2}})
+    (= (k-combinations 3 #{0 1 2 3 4}) #{#{0 1 2} #{0 1 3} #{0 1 4} #{0 2 3} #{0 2 4}
+                                         #{0 3 4} #{1 2 3} #{1 2 4} #{1 3 4} #{2 3 4}})
+    (= (k-combinations 4 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a "abc" "efg"}})
+    (= (k-combinations 2 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a} #{[1 2 3] "abc"} #{[1 2 3] "efg"}
+                                                      #{:a "abc"} #{:a "efg"} #{"abc" "efg"}})))
