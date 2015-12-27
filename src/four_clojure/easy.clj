2d3bc470fecc7a779d22943eeade1746798482ed
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-12-26 22:23:56 -0500

    Solve Problem #107

--- src/four_clojure/easy.clj
+++ src/four_clojure/easy.clj
@@ -460,3 +460,23 @@
     (== (least-common-multiple 3/4 1/6) 3/2)
     (== (least-common-multiple 7 5/7 2 3/5) 210)))
 
+(defn problem-one-hundred-seven
+  "Lexical scope and first-class functions are two of the most basic building blocks
+  of a functional language like Clojure.
+
+  When you combine the two together, you get something very powerful called lexical closures.
+  With these, you can exercise a great deal of control over the lifetime of your local bindings,
+  saving their values for use later, long after the code you're running now has finished.
+
+  It can be hard to follow in the abstract, so let's build a simple closure.
+  Given a positive integer n, return a function (f x) which computes x^n.
+  Observe that the effect of this is to preserve the value of n for use outside the scope in which it is defined."
+  []
+  (let [f (fn [n]
+            (fn [x]
+              (int
+                (Math/pow x n))))]
+    (= 256 ((f 2) 16),
+       ((f 8) 2))
+    (= [1 8 27 64] (map (f 3) [1 2 3 4]))
+    (= [1 2 4 8 16] (map #((f %) 2) [0 1 2 3 4]))))
