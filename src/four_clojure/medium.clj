826632de645f75eae3c90dc673679ad45c70050d
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-01-08 07:47:41 -0500

    Solve Problem #121

--- src/four_clojure/medium.clj
+++ src/four_clojure/medium.clj
@@ -695,3 +695,41 @@
     (= false (balanced-prime? 4))
     (= true (balanced-prime? 563))
     (= 1103 (nth (filter balanced-prime? (range)) 15))))
+
+(defn problem-one-hundred-twenty-one
+  "Given a mathematical formula in prefix notation, return a function that calculates the value of the formula.
+  The formula can contain nested calculations using the four basic mathematical operators, numeric constants,
+  and symbols representing variables. The returned function has to accept a single parameter containing
+  the map of variable names to their values."
+  []
+  (let [compute (fn compute [formula]
+                  (fn evaluate [parameters]
+                    (let [context (merge {'+ + '- - '* * '/ /} parameters)
+                          lookup (fn [symbol] (get context symbol))
+                          expression (map
+                                       #(cond
+                                         (symbol? %) (lookup %)
+                                         (list? %) ((compute %) parameters)
+                                         :else %)
+                                       formula)
+                          operator (first expression)
+                          arguments (rest expression)]
+                      (apply operator arguments))))]
+
+    (= 2 ((compute '(/ a b))
+
+           '{b 8 a 16}))
+
+    (= 8 ((compute '(+ a b 2))
+           '{a 2 b 4}))
+
+    (= [6 0 -4]
+       (map (compute '(* (+ 2 a)
+                         (- 10 b)))
+            '[{a 1 b 8}
+              {b 5 a -2}
+              {a 2 b 11}]))
+
+    (= 1 ((compute '(/ (+ x 2)
+                       (* 3 (+ y 1))))
+           '{x 4 y 1}))))
